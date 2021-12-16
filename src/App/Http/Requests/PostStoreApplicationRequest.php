95ff8485b13a97f6ce36a27ca69418510d307692
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2021-12-16 17:14:46 -0300

    publishable requests inplementsed

---
+++ src/App/Http/Requests/PostStoreApplicationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostStoreApplicationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'          => 'required|string',
+            'description'   => 'required|string',
+            'site'          => 'required|string',
+            'redirect_uri'  => 'required|string',
+            'logo'          => 'required|string',
+        ];
+    }
+}
